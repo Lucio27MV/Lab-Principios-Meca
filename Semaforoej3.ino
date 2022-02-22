bc8512b16e01daff642bc12a47e82f3dbcee0af9
Lucio27MV <47005249+Lucio27MV@users.noreply.github.com>
2022-02-22 15:05:40 -0600

    Update Semaforoej3.ino

--- Semaforoej3.ino
+++ Semaforoej3.ino
@@ -1,88 +1,32 @@
-
-void setup() {
-  // put your setup code here, to run once:
-  //semaforo 1
-  pinMode(11, OUTPUT);            //11 verde
-  pinMode(LED_BUILTIN, OUTPUT);   //12 amarillo
-  pinMode(13, OUTPUT);            //13 rojo
- 
-  //semaforo 2
-  pinMode(3, OUTPUT);            //3 verde
-  pinMode(4, OUTPUT);             //4 amarillo
-  pinMode(5, OUTPUT);            //5 rojo
+void setup()
+{
+  pinMode(13, OUTPUT); //verde semaforo 1
+  pinMode(12, OUTPUT);//amarillo semaforo 1
+  pinMode(11, OUTPUT);// ROJO semaforo 1
+  pinMode(10, OUTPUT);//verde semaforo 2
+  pinMode(9, OUTPUT);//amarillo semaforo 2
+  pinMode(8, OUTPUT);//rojo semaforo 2
 }
 
-
-void loop() {
-  //ESTADO 1
+void loop()
+{
+  digitalWrite(12, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, HIGH);
+  delay(5000);
  
-  //sem 1
-  digitalWrite(13,HIGH);   // enciende el led y high es el nivel de voltaje
-  delay(5000);                       // se espera un segundo
-  digitalWrite(LED_BUILTIN, LOW);    // apaga el led con el voltaje low
-  delay(5000);
-  digitalWrite(11, LOW);    // apaga el led con el voltaje low
-  delay(5000);
-
-  //sem 2
-  digitalWrite(5, LOW);   // enciende el led y high es el nivel de voltaje
-  delay(5000);                       // se espera un segundo
-  digitalWrite(4, LOW);    // apaga el led con el voltaje low
-  delay(5000);
-  digitalWrite(3, HIGH);    // apaga el led con el voltaje low
-  delay(5000);
-
-    //ESTADO 2
+  digitalWrite(10, LOW);
+  digitalWrite(9, HIGH);
+  delay(1000);
  
-  //sem 1
-  digitalWrite(13, HIGH);   // enciende el led y high es el nivel de voltaje
-  delay(1000);                       // se espera un segundo
-  digitalWrite(LED_BUILTIN, LOW);    // apaga el led con el voltaje low
-  delay(1000);
-  digitalWrite(11, LOW);    // apaga el led con el voltaje low
-  delay(1000);
-
-  //sem 2
-  digitalWrite(5, LOW);   // enciende el led y high es el nivel de voltaje
-  delay(1000);                       // se espera un segundo
-  digitalWrite(4, HIGH);    // apaga el led con el voltaje low
-  delay(1000);
-  digitalWrite(3, LOW);    // apaga el led con el voltaje low
-  delay(1000);
-
-    //ESTADO 3
+  digitalWrite(11, LOW);
+  digitalWrite(13, HIGH);
+  digitalWrite(9, LOW);
+  digitalWrite(8, HIGH);
+  delay(5000);
  
-  //sem 1
-  digitalWrite(13, LOW);   // enciende el led y high es el nivel de voltaje
-  delay(5000);                       // se espera un segundo
-  digitalWrite(LED_BUILTIN, LOW);    // apaga el led con el voltaje low
-  delay(5000);
-  digitalWrite(11, HIGH);    // apaga el led con el voltaje low
-  delay(5000);
-
-  //sem 2
-  digitalWrite(5, HIGH);   // enciende el led y high es el nivel de voltaje
-  delay(5000);                       // se espera un segundo
-  digitalWrite(4, LOW);    // apaga el led con el voltaje low
-  delay(5000);
-  digitalWrite(3, LOW);    // apaga el led con el voltaje low
-  delay(5000);
-
-      //ESTADO 4
- 
-  //sem 1
-  digitalWrite(13, LOW);   // enciende el led y high es el nivel de voltaje
-  delay(1000);                       // se espera un segundo
-  digitalWrite(LED_BUILTIN, HIGH);    // apaga el led con el voltaje low
+  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH);
   delay(1000);
-  digitalWrite(11, LOW);    // apaga el led con el voltaje low
-  delay(1000);
-
-  //sem 2
-  digitalWrite(5, HIGH);   // enciende el led y high es el nivel de voltaje
-  delay(1000);                       // se espera un segundo
-  digitalWrite(4, LOW);    // apaga el led con el voltaje low
-  delay(1000);
-  digitalWrite(3, LOW);    // apaga el led con el voltaje low
-  delay(1000);
- }
+}
