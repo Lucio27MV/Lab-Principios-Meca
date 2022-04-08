0a6647fe5beb36d550f2a786413d1d1e1fb9bcb0
Lucio27MV <47005249+Lucio27MV@users.noreply.github.com>
2022-04-08 15:59:01 -0500

    Add files via upload

---
+++ Práctica5_Robot Operating System/circuloRos.py
@@ -0,0 +1,106 @@
+#! /usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+import math
+import time
+from std_srvs.srv import Empty
+import numpy as np
+
+x = 0
+y = 0
+z = 0
+theta = 0
+
+def poseCallback(pose_message):
+    global x
+    global y
+    global z
+    global theta
+    
+    x = pose_message.x
+    y = pose_message.y
+    theta = pose_message.theta
+
+def orientate (xgoal, ygoal):
+    global x
+    global y
+    global theta
+
+    velocity_message = Twist()
+    cmd_vel_topic = '/turtle1/cmd_vel'
+
+    while(True):
+        ka = 1.0
+        desired_angle_goal = math.atan2(ygoal-y, xgoal-x)
+        if theta < 0:
+        	theta = theta + 2*math.pi
+        if desired_angle_goal < 0:
+        	desired_angle_goal = desired_angle_goal + 2*math.pi	
+        dtheta = desired_angle_goal-theta
+        if (abs(dtheta) < 0.1):
+        	time.sleep(1)
+        	break
+        angular_speed = ka * (dtheta)
+
+        velocity_message.linear.x = 0.0
+        velocity_message.angular.z = angular_speed
+        velocity_publisher.publish(velocity_message)
+        print ('x=', x, 'y=', y)
+
+def go_to_goal (xgoal, ygoal):
+    global x
+    global y
+    global theta
+    global radio
+    global radianes
+
+    velocity_message = Twist()
+    cmd_vel_topic = '/turtle1/cmd_vel'
+
+    while(True):
+        kv = 1.0				
+        distance = abs(math.sqrt(((xgoal-x)**2)+((ygoal-y)**2)))
+        linear_speed = kv * distance
+
+        ka = 5.0
+        desired_angle_goal = math.atan2(ygoal+0.01-y, xgoal-x)
+        if (desired_angle_goal < 0.0):	
+        	desired_angle_goal = desired_angle_goal + 6.28
+        dtheta = desired_angle_goal-theta        
+        angular_speed = ka * (dtheta)
+        if (abs(angular_speed) < 0.5):	
+        	angular_speed = angular_speed
+        else:
+        	angular_speed = 0.0
+
+        velocity_message.linear.x = linear_speed
+        velocity_message.angular.z = angular_speed
+        velocity_publisher.publish(velocity_message)
+        print ('x=', x, 'y=', y)
+
+        if (distance < 0.01):
+            time.sleep(1)
+            break
+
+if __name__ == '__main__':
+    try:
+
+        rospy.init_node('turtlesim_motion_pose', anonymous = True)
+
+        cmd_vel_topic = '/turtle1/cmd_vel'
+        velocity_publisher = rospy.Publisher(cmd_vel_topic, Twist, queue_size = 10)
+
+        position_topic = "/turtle1/pose"
+        pose_subscriber = rospy.Subscriber(position_topic, Pose, poseCallback)
+        time.sleep(2) 
+        
+        for radianes in np.linspace(0, 2*(math.pi), 50):       	
+        	x_deseada = 6 + 2*math.cos(radianes)
+        	y_deseada = 6 + 2*math.sin(radianes)
+        	orientate(x_deseada,y_deseada)
+        	go_to_goal(x_deseada,y_deseada)
+        
+    except rospy.ROSInterruptException:        
+    	pass
